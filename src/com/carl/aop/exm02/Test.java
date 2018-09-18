e2f914b54b5afd284f6ba5ae3a6967252c9e1be1
liugx <liugx618@163.com>
2018-09-18 09:10:29 +0800

    spring boot

--- src/com/carl/aop/exm02/Test.java
+++ src/com/carl/aop/exm02/Test.java
@@ -11,6 +11,9 @@ public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/carl/aop/exm02/beans.xml");
         Chinese c= context.getBean(Chinese.class);
+        if (c instanceof Chinese) {
+
+        }
         c.chifan();
         c.shuijiao();
         System.out.println("*********************************");
