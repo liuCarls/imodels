d597e71973266e55b7e158b751fe815043fb6a22
liugx <liugx618@163.com>
2018-12-07 15:20:32 +0800

    reflect 4 structure

--- src/com/carl/dates/DTTest.java
+++ src/com/carl/dates/DTTest.java
@@ -2,6 +2,8 @@ package com.carl.dates;
 
 import com.carl.ibatis.domain.User;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -10,16 +12,18 @@ import java.util.Date;
  */
 public class DTTest {
     public static void main(String[] args) {
-        Calendar lastT = Calendar.getInstance();
-        lastT.setTime(new Date("2018/10/12"));
-        lastT.add(Calendar.MINUTE, 5);
-        Date ps = lastT.getTime();
+//        Calendar lastT = Calendar.getInstance();
+//        lastT.setTime(new Date("2018/10/12"));
+//        lastT.add(Calendar.MINUTE, 5);
+//        Date ps = lastT.getTime();
 //
-        System.out.println("ps is "+ps);
-
-        System.out.println(ps.before(new Date()));
-        System.out.println(ps.after(new Date()));
-        System.out.println(lastT.after(new Date()));
+//        System.out.println("ps is "+ps);
+//
+//        System.out.println(ps.before(new Date()));
+//        System.out.println(ps.after(new Date()));
+//        System.out.println(lastT.after(new Date()));
+        DTTest thiz = new DTTest();
+        thiz.duration();
 //
 //        //-----------
 //        Date d1 = new Date();
@@ -36,5 +40,28 @@ public class DTTest {
             io = new User();
         }
         System.out.println(io);
+    }
+
+    public void duration() {
+//https://blog.csdn.net/sy793314598/article/details/79544796
+        //传统的SimpleDateFormat类
+
+        //java 7中的日历类Calendar
+
+        //java 8中的周期类Period
+
+        //java 8中的Duration类
+        Instant ins1 = Instant.now();
+
+        try {
+            Thread.sleep(1000*5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Instant ins2 = Instant.now();
+        System.out.println("以毫秒计的时间差：" + Duration.between(ins1, ins2).toMillis());
+
+        //java 8中的ChronoUnit类
     }
 }
--- src/com/carl/littleS/Tester.java
+++ src/com/carl/littleS/Tester.java
@@ -1,14 +1,52 @@
 package com.carl.littleS;
 
+import java.lang.reflect.Method;
+
 /**
  * Created by user on 2018/8/23.
  */
 public class Tester {
     public static void main(String[] args) {
-        Function102 fun102 = new Function102("sn102");
-        Function3 fun3 = new Function3("sn3");
-        StaticHandler.execute(fun102);
-        StaticHandler.execute(fun3);
+//        Function102 fun102 = new Function102("sn102");
+//        Function3 fun3 = new Function3("sn3");
+//        StaticHandler.execute(fun102);
+//        StaticHandler.execute(fun3);
 
+        // reflect .......
+        Tester thiz = new Tester();
+        Result res1 = thiz.getCObject(new Result());
+//        System.out.println(res1.status);
+        Result res2 = thiz.getCObject2(Result.class);
+        System.out.println(res2.status);
+    }
+
+
+    public <T> T getCObject(T t) {
+        Class<?> clazz = t.getClass();
+        System.out.println(clazz);
+//        Method[] methods = clazz.getMethods(); //包括继承的方法
+        Method[] methods = clazz.getDeclaredMethods(); //本类声明的方法
+//        Method method = clazz.getEnclosingMethod(); //
+//        System.out.println(method.getName());
+        for (Method md : methods) {
+            System.out.println(md.getName());
+
+        }
+        //怎么处理这个t呢
+        return null;
+    }
+
+    public <T extends Result> T getCObject2(Class T) {
+        Class[] obj = T.getInterfaces();
+        try {
+            Object obj2 = T.newInstance();
+            return (T) obj2;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        //怎么处理这个t呢
+        return null;
     }
 }
